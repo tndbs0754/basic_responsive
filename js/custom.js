1741c6d619782d1879faa457300eed363f607f5f
tndbs0754 <169110561+tndbs0754@users.noreply.github.com>
2024-05-27 16:24:22 +0900

    0527 기본 홈페이지

--- js/custom.js
+++ js/custom.js
@@ -1,44 +1,32 @@
 $(function () {
 
-
-    $('.main_content .right_slide').slick({
+    $('.main_visual_slide').slick({
         arrows: false,
-        dots: true
-    });
-
-
-    $('.main_content .arrows .prev').on('click', function () {
-        $('.main_content .right_slide').slick('slickPrev')
-    });
-
-    $('.main_content .arrows .next').on('click', function () {
-        $('.main_content .right_slide').slick('slickNext')
-    });
-
-    $('.main_solution .solution_slide').slick({
-        centerMode: true,
-        centerPadding: '300px',
         dots: true,
-        arrows: false
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
     });
 
-    $('.main_solution .solution_slide').on('afterChange', function (e, s, c) {
-        $('.main_solution .slide_pop .desc')
-            .eq(c)
-            .addClass('on')
-            .siblings()
-            .removeClass('on');
+    $('#bgndVideo').YTPlayer({
+        videoURL: 'https://youtu.be/TWlRhK8Ll-Q',
+        containment: '.main_movie',
+        showControls: false,
+        playOnlyIfVisible: true,
+    });
 
-        $('.main_solution .num span').text('0' + (c + 1))
+
+    let sw = true;
+
+    $('.main_movie .switch').on('click', function () {
+        $(this).toggleClass('on');
+        if (sw) {
+            $('#bgndVideo').YTPPause();
+        } else {
+            $('#bgndVideo').YTPPlay();
+        }
+        sw = !sw;
+
     })
-
-    $('.main_solution .arrows .prev').on('click', function () {
-        $('.main_solution .solution_slide').slick('slickPrev')
-    });
-
-    $('.main_solution .arrows .next').on('click', function () {
-        $('.main_solution .solution_slide').slick('slickNext')
-    });
-
 
 })
