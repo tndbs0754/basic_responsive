1d17b169c8eb5bad238a4df8e92db143d67163ed
tndbs0754 <169110561+tndbs0754@users.noreply.github.com>
2024-05-30 18:07:09 +0900

    홈페이지 수정본

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,10 @@
 $(function () {
 
-    $('.main_visual_slide').slick({
+    $('.main_visual .main_visual_slide').slick({
         arrows: false,
         dots: true,
         autoplay: true,
         pauseOnHover: false,
         pauseOnFocus: false,
-    });
-
-    $('#bgndVideo').YTPlayer({
-        videoURL: 'https://youtu.be/TWlRhK8Ll-Q',
-        containment: '.main_movie',
-        showControls: false,
-        playOnlyIfVisible: true,
-    });
-
-
-    let sw = true;
-
-    $('.main_movie .switch').on('click', function () {
-        $(this).toggleClass('on');
-        if (sw) {
-            $('#bgndVideo').YTPPause();
-        } else {
-            $('#bgndVideo').YTPPlay();
-        }
-        sw = !sw;
-
     })
-
 })
