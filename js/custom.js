5d4d4bff0d58b469274c3d9f7b791a274da21b95
tndbs0754 <169110561+tndbs0754@users.noreply.github.com>
2024-06-10 16:24:36 +0900

    독도체험관

--- js/custom.js
+++ js/custom.js
@@ -1,60 +1,23 @@
 $(function () {
 
-    $('.main_visual .main_visual_slide').slick({
-        arrows: false,
-        dots: true,
-        autoplay: true,
-        pauseOnHover: false,
-        pauseOnFocus: false,
-    });
-
-    $("#bgndVideo").YTPlayer({
-        videoURL: 'https://youtu.be/d3go9ig7-zs',
-        containment: '.main_movie',
-        showControls: false,
-        playOnlyIfVisible: true,
-    });
-
-    let sw = true;
-
-    $('.main_movie .swich').on('click', function () {
-        $(this).toggleClass('on');
-        if (sw) {
-            $('#bgndVideo').YTPPause();
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        if (sct > 0) {
+            $('#header').addClass('on')
         } else {
-            $('#bgndVideo').YTPPlay();
+            $('#header').removeClass('on')
         }
-        sw = !sw;
     })
 
 
-    $('.main_product_slide').slick({
-        slidesToShow: 4,
-        arrows: false,
-        dots: true,
-    });
 
-
-    $('.main_product .arrows .prev').on('click', function () {
-        $('.main_product_slide').slick('slickPrev')
+    const mainVisualSlide = new Swiper('.main_visual_slide', {
+        parallax: true,
+        loop: true,
+        speed: 1000,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
     });
-
-    $('.main_product .arrows .next').on('click', function () {
-        $('.main_product_slide').slick('slickNext')
-    });
-
-    $('.main_event_slide').slick({
-        slidesToShow: 5,
-        centerMode: true,
-        centerPadding: 0,
-        arrows: false,
-    });
-
-    $('.main_event .arrows .prev').on('click', function () {
-        $('.main_event_slide').slick('slickPrev')
-    });
-    $('.main_event .arrows .next').on('click', function () {
-        $('.main_event_slide').slick('slickNext')
-    });
-
 })
